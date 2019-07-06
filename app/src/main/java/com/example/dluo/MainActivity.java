3ffe425584b9f174ee5e67121af3565f0ccd3a94
Gabriel <gbrljorge@gmail.com>
2019-07-06 18:15:54 +0200

    Requette HTTP basique

--- app/src/main/java/com/example/dluo/MainActivity.java
+++ app/src/main/java/com/example/dluo/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.dluo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-//MODIFICATION A LA CON
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Test
+        Network.test(this);
     }
 }
