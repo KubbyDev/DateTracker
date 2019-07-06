5123019082a96749ebef57babeab580b6bc88c78
adiflex <d.jorge91@gmail.com>
2019-07-06 17:48:49 +0200

    la con
    la con

--- app/src/main/java/com/example/dluo/MainActivity.java
+++ app/src/main/java/com/example/dluo/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.dluo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//MODIFICATION A LA CON
 public class MainActivity extends AppCompatActivity {
 
     @Override
